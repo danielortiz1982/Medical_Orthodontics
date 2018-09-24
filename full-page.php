6f9dd48528796f9267faaa217261c6135ee77d9f
dortiz <daniel.ortiz@teachforamerica.org>
2018-09-24 17:19:29 -0400

    sass update

---
+++ full-page.php
@@ -0,0 +1,22 @@
+<?php /* Template Name: Full Page */ ?>
+<?php get_header(); ?>
+<?php if(have_posts() ) : while(have_posts() ) : the_post(); ?>
+<section class="bv-row">
+	<div class="main-content" id="full-page">
+			<div class="feature-img">
+				<a href="<?php the_permalink(); ?>"><img src="<?php the_post_thumbnail_url(); ?>" /></a>
+			</div>
+		<div class="bv-container">
+			<h2 class="the-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+			<div class="bv-row">
+				<div class="bv-12">
+					<div class="the-content"><?php the_content(); ?></div>
+				</div>
+			</div>
+			<?php endwhile ?>
+			<?php endif ?>
+		</div>
+	</div>
+
+</section>
+<?php get_footer(); ?>
